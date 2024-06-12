32cfdc4df5bb53aa73635a413627d144efa61b91
lanpham19993 <lanpham09111993@outlook.com.vn>
2024-06-12 12:30:07 +0200

    commit testcase Add to cart

---
+++ src/test/java/com/w2a/testcases/AddToCartTest.java
@@ -0,0 +1,24 @@
+package com.w2a.testcases;
+
+import org.testng.annotations.Test;
+
+import pages.DetailPage;
+import pages.HomePage;
+
+public class AddToCartTest extends BaseTest{
+
+	HomePage home = new HomePage();
+	@Test
+	public void addToCartFromProductPage() throws InterruptedException {
+		home.AddToCart("btnFleeceJacket_id");
+	}
+	
+	@Test
+	public void addToCartFromDetailPage() throws InterruptedException {
+		DetailPage detail =  home.goToDetail("linkTshirtRed_id");
+		Thread.sleep(500);
+		detail.AddToCart();
+		Thread.sleep(500);
+		detail.backToHomePage();
+	}
+}
